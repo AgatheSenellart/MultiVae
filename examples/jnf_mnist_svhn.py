1fd993c959f9ae44ebd68fd21c14235c6a6a4708
AgatheSenellart <agathe.senellart@polytechnique.edu>
2023-02-24 16:00:45 +0100

    add example for jnf training

--- examples/jnf_mnist_svhn.py
+++ examples/jnf_mnist_svhn.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader, random_split
 from multivae.data.datasets import MnistSvhn
 from multivae.data.datasets.utils import save_all_images
 from multivae.data.utils import set_inputs_to_device
-from multivae.models import JMVAE, JMVAEConfig
+from multivae.models import JNF, JNFConfig
 from multivae.models.nn.default_architectures import Decoder_AE_MLP, Encoder_VAE_MLP
 from multivae.models.nn.svhn import Decoder_VAE_SVHN, Encoder_VAE_SVHN
 from multivae.trainers import BaseTrainer, BaseTrainerConfig
@@ -20,11 +20,12 @@ train_data, eval_data = random_split(
     train_data, [0.8, 0.2], generator=torch.Generator().manual_seed(42)
 )
 
-model_config = JMVAEConfig(
+model_config = JNFConfig(
     n_modalities=2,
     input_dims=dict(mnist=(1, 28, 28), svhn=(3, 32, 32)),
     latent_dim=20,
     warmup=100,
+    use_likelihood_rescaling=True
 )
 
 encoders = dict(
@@ -37,7 +38,7 @@ decoders = dict(
     svhn=Decoder_VAE_SVHN(BaseAEConfig(latent_dim=20, input_dim=(3, 32, 32))),
 )
 
-model = JMVAE(model_config, encoders, decoders)
+model = JNF(model_config, encoders, decoders)
 
 trainer_config = BaseTrainerConfig(num_epochs=200, learning_rate=1e-3, steps_predict=5)
 
