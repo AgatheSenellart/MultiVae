2352b6e37493544f2e1510b41bfd052a7e6f02d9
clementchadebec <clement.chadebec@orange.fr>
2023-04-12 16:59:22 +0200

    fix test issue

--- tests/test_mnist_svhn_dataset.py
+++ tests/test_mnist_svhn_dataset.py
@@ -8,16 +8,17 @@ from multivae.data.datasets.base import MultimodalBaseDataset
 from multivae.data.datasets.mnist_svhn import MnistSvhn
 
 
+@pytest.mark.slow
 class Test:
     @pytest.fixture
-    def input_dataset_test(self):
-        data_path = "../data"
+    def input_dataset_test(self, tmpdir):
+        data_path = os.path.join(tmpdir, "data")
         split = "test"
 
         return dict(data_path=data_path, split=split)
 
     def test_create_dataset(self, input_dataset_test):
-        dataset = MnistSvhn(**input_dataset_test)
+        dataset = MnistSvhn(**input_dataset_test, download=True)
         assert isinstance(dataset, MultimodalBaseDataset)
         sample = dataset[0]
         assert type(sample) == DatasetOutput
