a1c53c6390d057e1cf9474f9ca184b1f8e4eeb9b
Jean Senellart <jean.senellart@quandela.com>
2023-06-07 23:10:10 +0200

    enable download by default and create directory

--- examples/mmnist/mvtcae.py
+++ examples/mmnist/mvtcae.py
@@ -12,7 +12,7 @@ from multivae.trainers.base.callbacks import (
     WandbCallback,
 )
 
-train_data = MMNISTDataset(data_path="../../../data/MMNIST", split="train")
+train_data = MMNISTDataset(data_path="../../../data/MMNIST/", split="train", download=True)
 train_data, eval_data = random_split(
     train_data, [0.8, 0.2], generator=torch.Generator().manual_seed(42)
 )
