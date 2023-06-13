9cef5b5beec482d970a63681e94ab7522240712e
Agathe <agathe.senellart@polytechnique.edu>
2023-06-13 17:36:11 +0200

    add tutorial notebook

--- src/multivae/data/datasets/__init__.py
+++ src/multivae/data/datasets/__init__.py
@@ -2,7 +2,7 @@
 In this section, you will find all the `built-in` datasets that are currently implemented in `multivae` library
 """
 
-from .base import IncompleteDataset, MultimodalBaseDataset
+from .base import IncompleteDataset, MultimodalBaseDataset, DatasetOutput
 from .mnist_svhn import MnistSvhn
 
-__all__ = ["MultimodalBaseDataset", "MnistSvhn", "IncompleteDataset"]
+__all__ = ["MultimodalBaseDataset", "MnistSvhn", "IncompleteDataset", "DatasetOutput"]
