521e944c7b6beaf4e0fa5ff961c6ac7dbb267c87
Agathe <agathe.senellart@polytechnique.edu>
2023-08-08 19:44:01 +0200

    compute joint reconstruction

--- mmnist_with_missing/training/convnets/compute_inference_time.py
+++ mmnist_with_missing/training/convnets/compute_inference_time.py
@@ -1,6 +1,5 @@
 """Script to compute more evaluation metrics on the MMNIST experiments"""
 
-
 import argparse
 import json
 import os
@@ -33,46 +32,42 @@ small_batch = next(iter(DataLoader(test_set,10)))
 
 for model_name in ['JMVAE', 'JNF', 'JNFDcca', 'MMVAE', 'MoPoE', 'MVTCAE']:
 
-    missing_ratio = "0"
-    incomplete = "c"
-    seed = 0
-    
+    for missing_ratio in ["0", "02","05"]:
+        for incomplete in ["c", "i"]:
+            if incomplete=='i' and missing_ratio==0:
+                continue
+            seed = 0
+
+            hf_repo = f"asenella/mmnist_{model_name}{config_name}_seed_{seed}_ratio_{missing_ratio}_{incomplete}"
+            model = AutoModel.load_from_hf_hub(hf_repo, allow_pickle=True)
+            
+            # wandb configuration
+            id = f'{model_name}_{incomplete}_{missing_ratio}_{seed}'
+            wandb_run = wandb.init(entity="multimodal_vaes",
+                                project='reconstruction_mmnist',
+                                config=model.model_config.to_dict()
+                                )
 
 
+            if torch.cuda.is_available():   
+                model = model.cuda()
+                model.device = "cuda"
+            else :
+                model.cpu()
+                model.device ='cpu'
 
+            vis_config = VisualizationConfig(
+                batch_size=128,
+                wandb_path=wandb_run.path,
+                n_samples=8,
+                n_data_cond=1
+            )
+            model
 
-    hf_repo = f"asenella/mmnist_{model_name}{config_name}_seed_{seed}_ratio_{missing_ratio}_{incomplete}"
-    model = AutoModel.load_from_hf_hub(hf_repo, allow_pickle=True)
-    
-    # wandb configuration
-    id = f'{model_name}_{incomplete}_{missing_ratio}_{seed}'
-    wandb_run = wandb.init(entity="multimodal_vaes",
-                        project='inference_time_mmnist',
-                        config=model.model_config.to_dict()
-                        )
-
-
-    if torch.cuda.is_available():   
-        model = model.cuda()
-        model.device = "cuda"
-    else :
-        model.cpu()
-        model.device ='cpu'
-
-
-
-    # Compute cross-modal inference time depending on the number of input modalities
-    
-    for i in range(1,6):
-        cond_mod = modalities[:i]
-        t1 = time.time()
-        model.predict(small_batch, cond_mod=cond_mod)
-        t2 = time.time()
-        
-        wandb.log(
-            {
-                'inference_time_with_{i}_modalities' : t2-t1
-            }
-        )
-    
-    wandb_run.finish()
+            Visualization(model,
+                          test_dataset=test_set,
+                          output=f'/home/asenella/scratch/reconstruction_mmnist/{model.model_name}/{incomplete}/{missing_ratio}',
+                          eval_config=vis_config
+                          ).conditional_samples_subset(subset='all')
+            
+            wandb_run.finish()
