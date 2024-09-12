98e73287cf8062acf09e6eb56bdae77f4513d8a3
AgatheSenellart <agathe.senellart@gmail.com>
2024-09-12 11:27:35 +0200

    try to train on CUB

---
+++ cub/architectures_text.py
@@ -0,0 +1,185 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from multivae.models.base import BaseEncoder, BaseDecoder, ModelOutput
+
+class Constants(object):
+    eta = 1e-20
+
+# Constants
+maxSentLen = 32  # max length of any description for birds dataset
+minOccur = 3
+embeddingDim = 128
+lenWindow = 3
+fBase = 32
+vocabSize = 1590
+
+
+# Classes
+class Enc(BaseEncoder):
+    """ Generate latent parameters for sentence data. """
+
+    def __init__(self, latentDim_w, latentDim_z, dist):
+        super(Enc, self).__init__()
+        self.latent_dim=latentDim_z
+        self.dist = dist
+        self.embedding = nn.Linear(vocabSize, embeddingDim)
+        self.enc_w = nn.Sequential(
+            # input size: 1 x 32 x 128
+            nn.Conv2d(1, fBase, 4, 2, 1, bias=True),
+            nn.BatchNorm2d(fBase),
+            nn.ReLU(True),
+            # size: (fBase) x 16 x 64
+            nn.Conv2d(fBase, fBase * 2, 4, 2, 1, bias=True),
+            nn.BatchNorm2d(fBase * 2),
+            nn.ReLU(True),
+            # size: (fBase * 2) x 8 x 32
+            nn.Conv2d(fBase * 2, fBase * 4, 4, 2, 1, bias=True),
+            nn.BatchNorm2d(fBase * 4),
+            nn.ReLU(True)
+        )
+        self.enc_z = nn.Sequential(
+            # input size: 1 x 32 x 128
+            nn.Conv2d(1, fBase, 4, 2, 1, bias=True),
+            nn.BatchNorm2d(fBase),
+            nn.ReLU(True),
+            # size: (fBase) x 16 x 64
+            nn.Conv2d(fBase, fBase * 2, 4, 2, 1, bias=True),
+            nn.BatchNorm2d(fBase * 2),
+            nn.ReLU(True),
+            # size: (fBase * 2) x 8 x 32
+            nn.Conv2d(fBase * 2, fBase * 4, 4, 2, 1, bias=True),
+            nn.BatchNorm2d(fBase * 4),
+            nn.ReLU(True),
+            # # size: (fBase * 4) x 4 x 16
+            nn.Conv2d(fBase * 4, fBase * 8, (1, 4), (1, 2), (0, 1), bias=True),
+            nn.BatchNorm2d(fBase * 8),
+            nn.ReLU(True),
+            # size: (fBase * 8) x 4 x 8
+            nn.Conv2d(fBase * 8, fBase * 16, (1, 4), (1, 2), (0, 1), bias=True),
+            nn.BatchNorm2d(fBase * 16),
+            nn.ReLU(True),
+            # size: (fBase * 8) x 4 x 4
+        )
+        self.c1_w = nn.Linear(fBase * 16 * 16, latentDim_w)
+        self.c2_w = nn.Linear(fBase * 16 * 16, latentDim_w)
+
+        self.c1_z = nn.Conv2d(fBase * 16, latentDim_z, 4, 1, 0, bias=True)
+        self.c2_z = nn.Conv2d(fBase * 16, latentDim_z, 4, 1, 0, bias=True)
+
+        # self.ll_c1_z = nn.Sequential(*[nn.ReLU(), nn.Linear(128, latentDim_z)])
+        # self.ll_c2_z = nn.Sequential(*[nn.ReLU(), nn.Linear(128, latentDim_z)])
+        # self.ll_c1_w = nn.Sequential(*[nn.ReLU(), nn.Linear(128, latentDim_w)])
+        # self.ll_c2_w = nn.Sequential(*[nn.ReLU(), nn.Linear(128, latentDim_w)])
+
+    def forward(self, x):
+        x_emb = self.embedding(x).unsqueeze(1)
+        e_w = self.enc_w(x_emb)
+        e_w = e_w.view(-1, fBase * 16 * 16)
+        mu_w, lv_w = self.c1_w(e_w), self.c2_w(e_w)
+        e_z = self.enc_z(x_emb)
+        
+        if x.shape[0] == 1:
+            mu_z, lv_z = self.c1_z(e_z).squeeze().unsqueeze(0), self.c2_z(e_z).squeeze().unsqueeze(0)
+        else :
+            mu_z, lv_z = self.c1_z(e_z).squeeze(), self.c2_z(e_z).squeeze()
+        
+        if self.dist == 'Normal':
+            return ModelOutput(
+                embedding = mu_z,
+                style_embedding = mu_w,
+                log_covariance =  F.softplus(lv_z) + Constants.eta,
+                style_log_covariance =F.softplus(lv_w) + Constants.eta)
+                
+                
+        else:
+            return ModelOutput(
+                embedding = mu_z,
+                style_embedding = mu_w,
+                log_covariance = F.softmax(lv_z, dim=-1) * lv_w.size(-1) + Constants.eta,
+                style_log_covariance = F.softmax(lv_w, dim=-1) * lv_w.size(-1) + Constants.eta
+            )
+        
+       
+
+
+class Dec(BaseDecoder):
+    """ Generate a sentence given a sample from the latent space. """
+
+    def __init__(self, latentDim_w, latentDim_z):
+        
+        super(Dec, self).__init__()
+        self.latent_dim = latentDim_z
+        self.dec_w = nn.Sequential(
+            nn.ConvTranspose2d(latentDim_w, fBase * 16, 4, 1, 0, bias=True),
+            nn.BatchNorm2d(fBase * 16),
+            nn.ReLU(True),
+            # size: (fBase * 8) x 4 x 4
+            nn.ConvTranspose2d(fBase * 16, fBase * 8, (1, 4), (1, 2), (0, 1), bias=True),
+            nn.BatchNorm2d(fBase * 8),
+            nn.ReLU(True),
+            # size: (fBase * 8) x 4 x 8
+            nn.ConvTranspose2d(fBase * 8, fBase * 4, (1, 4), (1, 2), (0, 1), bias=True),
+            nn.BatchNorm2d(fBase * 4),
+            nn.ReLU(True),
+        )
+        self.dec_z = nn.Sequential(
+            nn.ConvTranspose2d(latentDim_z, fBase * 16, 4, 1, 0, bias=True),
+            nn.BatchNorm2d(fBase * 16),
+            nn.ReLU(True),
+            # size: (fBase * 8) x 4 x 4
+            nn.ConvTranspose2d(fBase * 16, fBase * 8, (1, 4), (1, 2), (0, 1), bias=True),
+            nn.BatchNorm2d(fBase * 8),
+            nn.ReLU(True),
+            # size: (fBase * 8) x 4 x 4
+            nn.ConvTranspose2d(fBase * 8, fBase * 8, 3, 1, 1, bias=True),
+            nn.BatchNorm2d(fBase * 8),
+            nn.ReLU(True),
+            # size: (fBase * 8) x 4 x 8
+            nn.ConvTranspose2d(fBase * 8, fBase * 4, (1, 4), (1, 2), (0, 1), bias=True),
+            nn.BatchNorm2d(fBase * 4),
+            nn.ReLU(True),
+        )
+        self.dec_h = nn.Sequential(
+            nn.ConvTranspose2d(fBase * 8, fBase * 4, 3, 1, 1, bias=True),
+            nn.BatchNorm2d(fBase * 4),
+            nn.ReLU(True),
+            # size: (fBase * 4) x 8 x 32
+            nn.ConvTranspose2d(fBase * 4, fBase * 2, 4, 2, 1, bias=True),
+            nn.BatchNorm2d(fBase * 2),
+            nn.ReLU(True),
+            # size: (fBase * 2) x 16 x 64
+            nn.ConvTranspose2d(fBase * 2, fBase, 4, 2, 1, bias=True),
+            nn.BatchNorm2d(fBase),
+            nn.ReLU(True),
+            # size: (fBase) x 32 x 128
+            nn.ConvTranspose2d(fBase, 1, 4, 2, 1, bias=True),
+            nn.ReLU(True)
+            # Output size: 1 x 64 x 256
+        )
+        # inverts the 'embedding' module upto one-hotness
+        self.toVocabSize = nn.Linear(embeddingDim, vocabSize)
+
+        self.latent_dim_w = latentDim_w
+        self.latent_dim_z = latentDim_z
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, u):
+        # z = z.unsqueeze(-1).unsqueeze(-1)  # fit deconv layers
+        w, z = torch.split(u, [self.latent_dim_w, self.latent_dim_z], dim=-1)
+        z = z.unsqueeze(-1).unsqueeze(-1)
+        hz = self.dec_z(z.view(-1, *z.size()[-3:]))
+        w = w.unsqueeze(-1).unsqueeze(-1)
+        hw = self.dec_w(w.view(-1, *w.size()[-3:]))
+        h = torch.cat((hw, hz), dim=1)
+        out = self.dec_h(h)
+        out = out.view(*z.size()[:-3], *out.size()[1:]).view(-1, embeddingDim)
+        # The softmax is key for this to work
+        ret = self.softmax(self.toVocabSize(out).view(*z.size()[:-3], maxSentLen, vocabSize))
+        return ModelOutput(reconstruction = ret)
+    
+    
+    
+    
+    
---
+++ cub/dataset.py
@@ -0,0 +1,286 @@
+# from multivae.data.datasets import MultimodalBaseDataset
+import io
+import json
+import os
+import pickle
+from collections import Counter, OrderedDict
+from collections import defaultdict
+import numpy as np
+import torch
+import torch.nn as nn
+from nltk.tokenize import sent_tokenize, word_tokenize
+from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
+
+class OrderedCounter(Counter, OrderedDict):
+    """Counter that remembers the order elements are first encountered."""
+
+    def __repr__(self):
+        return '%s(%r)' % (self.__class__.__name__, OrderedDict(self))
+
+    def __reduce__(self):
+        return self.__class__, (OrderedDict(self),)
+
+
+class CUBSentences(Dataset):
+
+    def __init__(self, root_data_dir, split, one_hot=False, transpose=False, transform=None, **kwargs):
+        """split: 'trainval' or 'test' """
+
+        super().__init__()
+        self.data_dir = os.path.join(root_data_dir, 'cub')
+        self.split = split
+        self.max_sequence_length = kwargs.get('max_sequence_length', 32)
+        self.min_occ = kwargs.get('min_occ', 3)
+        self.transform = transform
+        self.one_hot = one_hot
+        self.transpose = transpose
+        # os.makedirs(os.path.join(root_data_dir, "lang_emb"), exist_ok=True)
+
+        self.gen_dir = os.path.join(self.data_dir, "oc:{}_msl:{}".
+                                    format(self.min_occ, self.max_sequence_length))
+
+        if split == 'train':
+            self.raw_data_path = os.path.join(self.data_dir, 'text_trainvalclasses.txt')
+        elif split == 'test':
+            self.raw_data_path = os.path.join(self.data_dir, 'text_testclasses.txt')
+        else:
+            raise Exception("Only train or test split is available")
+
+        os.makedirs(self.gen_dir, exist_ok=True)
+        self.data_file = 'cub.{}.s{}'.format(split, self.max_sequence_length)
+        self.vocab_file = 'cub.vocab'
+
+        if not os.path.exists(os.path.join(self.gen_dir, self.data_file)):
+            print("Data file not found for {} split at {}. Creating new... (this may take a while)".
+                  format(split.upper(), os.path.join(self.gen_dir, self.data_file)))
+            self._create_data()
+
+        else:
+            self._load_data()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        sent = self.data[str(idx)]['idx']
+
+        if self.one_hot:
+            sent = nn.functional.one_hot(torch.Tensor(sent).long(), self.vocab_size).float()
+
+        if self.transpose:
+            sent = sent.transpose(-2, -1)
+        if self.transform is not None:
+            sent = self.transform(sent)
+        return sent, self.data[str(idx)]['length']
+
+    @property
+    def vocab_size(self):
+        return len(self.w2i)
+
+    @property
+    def pad_idx(self):
+        return self.w2i['<pad>']
+
+    @property
+    def eos_idx(self):
+        return self.w2i['<eos>']
+
+    @property
+    def unk_idx(self):
+        return self.w2i['<unk>']
+
+    def get_w2i(self):
+        return self.w2i
+
+    def get_i2w(self):
+        return self.i2w
+
+    def _load_data(self, vocab=True):
+        try:
+            with open(os.path.join(self.gen_dir, self.data_file), 'rb') as file:
+                self.data = json.load(file)
+        except TypeError:
+            with open(os.path.join(self.gen_dir, self.data_file), 'r') as file:
+                self.data = json.load(file)
+
+        if vocab:
+            self._load_vocab()
+
+    def _load_vocab(self):
+        if not os.path.exists(os.path.join(self.gen_dir, self.vocab_file)):
+            self._create_vocab()
+        with open(os.path.join(self.gen_dir, self.vocab_file), 'r') as vocab_file:
+            vocab = json.load(vocab_file)
+        self.w2i, self.i2w = vocab['w2i'], vocab['i2w']
+
+    def _create_data(self):
+        if self.split == 'train' and not os.path.exists(os.path.join(self.gen_dir, self.vocab_file)):
+            self._create_vocab()
+        else:
+            self._load_vocab()
+
+        with open(self.raw_data_path, 'r') as file:
+            text = file.read()
+            sentences = sent_tokenize(text)
+
+        data = defaultdict(dict)
+        pad_count = 0
+
+        for i, line in enumerate(sentences):
+            words = word_tokenize(line)
+
+            tok = words[:self.max_sequence_length - 1]
+            tok = tok + ['<eos>']
+            length = len(tok)
+            if self.max_sequence_length > length:
+                tok.extend(['<pad>'] * (self.max_sequence_length - length))
+                pad_count += 1
+            idx = [self.w2i.get(w, self.w2i['<exc>']) for w in tok]
+
+            id = len(data)
+            data[id]['tok'] = tok
+            data[id]['idx'] = idx
+            data[id]['length'] = length
+
+        print("{} out of {} sentences are truncated with max sentence length {}.".
+              format(len(sentences) - pad_count, len(sentences), self.max_sequence_length))
+        with io.open(os.path.join(self.gen_dir, self.data_file), 'wb') as data_file:
+            data = json.dumps(data, ensure_ascii=False)
+            data_file.write(data.encode('utf8', 'replace'))
+
+        self._load_data(vocab=False)
+
+    def _create_vocab(self):
+
+        import nltk
+        nltk.download('punkt')
+
+        assert self.split == 'train', "Vocablurary can only be created for training file."
+
+        with open(self.raw_data_path, 'r') as file:
+            text = file.read()
+            sentences = sent_tokenize(text)
+
+        occ_register = OrderedCounter()
+        w2i = dict()
+        i2w = dict()
+
+        special_tokens = ['<exc>', '<pad>', '<eos>']
+        for st in special_tokens:
+            i2w[len(w2i)] = st
+            w2i[st] = len(w2i)
+
+        texts = []
+        unq_words = []
+
+        for i, line in enumerate(sentences):
+            words = word_tokenize(line)
+            occ_register.update(words)
+            texts.append(words)
+
+        for w, occ in occ_register.items():
+            if occ > self.min_occ and w not in special_tokens:
+                i2w[len(w2i)] = w
+                w2i[w] = len(w2i)
+            else:
+                unq_words.append(w)
+
+        assert len(w2i) == len(i2w)
+
+        print("Vocablurary of {} keys created, {} words are excluded (occurrence <= {})."
+              .format(len(w2i), len(unq_words), self.min_occ))
+
+        vocab = dict(w2i=w2i, i2w=i2w)
+        with io.open(os.path.join(self.gen_dir, self.vocab_file), 'wb') as vocab_file:
+            data = json.dumps(vocab, ensure_ascii=False)
+            vocab_file.write(data.encode('utf8', 'replace'))
+
+        with open(os.path.join(self.gen_dir, 'cub.unique'), 'wb') as unq_file:
+            pickle.dump(np.array(unq_words), unq_file)
+
+        with open(os.path.join(self.gen_dir, 'cub.all'), 'wb') as a_file:
+            pickle.dump(occ_register, a_file)
+
+        self._load_vocab()
+
+    def one_hot_to_string(self, data):
+        ret_list = [self._to_string(i) for i in data]
+        return ret_list
+
+    def _to_string(self, matrix):
+        words = []
+
+        if self.transpose:
+            matrix = matrix.T
+
+        for i in range(matrix.shape[0]):
+            idx = np.argmax(matrix[i, :])
+            words.append(self.i2w[str(idx)])
+
+        ret_str = " ".join(words)
+        return ret_str
+
+# This is required because there are 10 captions per image.
+# Allows easier reuse of the same image for the corresponding set of captions.
+def resampler(dataset, idx):
+    return idx // 10
+
+
+from multivae.data.datasets.base import MultimodalBaseDataset, DatasetOutput
+from torchvision import transforms, datasets
+
+
+class CUB(MultimodalBaseDataset):
+    
+    def __init__(self, root_data_dir, split='train',max_lenght = 32):
+        
+        
+        transform_text = lambda data: torch.Tensor(data)
+        self.text_data = CUBSentences(root_data_dir,split,one_hot=True,transpose=False,transform=transform_text, max_sequence_length=max_lenght)
+        tx = transforms.Compose([transforms.Resize([64, 64]), transforms.ToTensor()])
+        self.image_data = datasets.ImageFolder(os.path.join(root_data_dir, 'cub',split), transform=tx)
+        
+        
+    def __getitem__(self, index):
+        
+        image = self.image_data[index // 10][0]
+        text = self.text_data[index][0]
+        
+        return DatasetOutput(data = dict(
+            image = image,
+            text = text)
+        )
+    
+    def __len__(self):
+        return len(self.text_data)
+
+
+    def plot_text(self,input_tensor, fig_size=(40,40)):
+        # input_tensor is of shape (max_sequence_lenght,vocab_size)
+        array = input_tensor.detach().numpy()
+        sentence = self.text_data._to_string(array)
+        
+        fig = plt.figure(figsize=fig_size)
+        plt.text(
+            x=0.5,
+            y=0.5,
+            s='{}'.format(
+                ' '.join(i + '\n' if (n + 1) % 1 == 0
+                         else i for n, i in enumerate([word for word in sentence.split() if word != '<eos>']))),
+            fontsize=7,
+            verticalalignment='center_baseline',
+            horizontalalignment='right'
+        )
+        plt.axis('off')
+
+        # Draw the canvas and retrieve the image as a NumPy array
+        fig.canvas.draw()
+        image_np = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+        image_np = image_np.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+        return image_np
+    
+    
+        
+    
