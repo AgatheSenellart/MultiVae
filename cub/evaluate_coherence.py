2b11b8560a5fbe9ed4dcb7cb2ea17c216bac869c
Agathe <agathe.senellart@polytechnique.edu>
2024-10-01 18:46:11 +0200

    evaluate_coherence

--- cub/evaluate_coherence.py
+++ cub/evaluate_coherence.py
@@ -11,12 +11,9 @@ from dataset import CUB, MultimodalBaseDataset
 import torch
 from multivae.models import AutoModel
 from multivae.metrics import Visualization, VisualizationConfig
-
-test_data = CUB('/home/asenella/scratch/data', split='test',max_lenght=32).text_data
-
-
-model = AutoModel.load_from_folder('/home/asenella/scratch/experiments/CUB/JNF_training_2024-09-20_10-59-58/final_model')
-
+import os
+import wandb
+from multivae.trainers.base.callbacks import load_wandb_path_from_folder
 
 def in_range(hsv, lower, upper):
     mask_h = np.logical_and(hsv[:,:,0] >= lower[0], hsv[:,:,0] <= upper[0])
@@ -24,8 +21,6 @@ def in_range(hsv, lower, upper):
     mask_v = np.logical_and(hsv[:,:,2] >= lower[2] , hsv[:,:,2] <= upper[2])
     
     return mask_h & mask_s & mask_v
-
-
 
 def count_pixels_per_color(hsv_image):
     
@@ -54,68 +49,90 @@ def count_pixels_per_color(hsv_image):
         
     return color_masks, color_count, most_present
 
-# Generate sentences and transform it 
-nb_coherent = 0
-nb_image = 0
-for color in ['white', 'yellow', 'red','blue', 'green','grey', 'brown', 'black']:
-    
-    sentence = f'This bird is completely {color}'
-    
-    tokenized = word_tokenize(sentence)
-    
-    # Then we need to transform to the same format as in the CUB dataset
-    tok = tokenized + ['<eos>']
-    length = len(tok)
-    pad_count = 0
-    if test_data.max_sequence_length > length:
-        tok.extend(['<pad>'] * (test_data.max_sequence_length - length))
-        pad_count += 1
-    idx = [test_data.w2i.get(w, test_data.w2i['<exc>']) for w in tok]
-    print(tokenized)
-    print(idx)
-    sent = torch.nn.functional.one_hot(torch.Tensor(idx).long(), test_data.vocab_size).float().unsqueeze(0)
-    
-    testing_set = MultimodalBaseDataset(data=dict(text = sent))
-    
-    # Visualize samples
-    vis_config = VisualizationConfig(n_samples=10,n_data_cond=1)
-    vis_module = Visualization(model,test_dataset=testing_set,output=f'./test/{color}', eval_config=vis_config)
-    
-    vis_module.conditional_samples_subset(['text'],gen_mod='image')
-    
-    
-    # Compute coherence metric
-    model.eval()
-    output = model.predict(inputs = MultimodalBaseDataset(data=dict(text = sent)),cond_mod='text',gen_mod='image', N=10) # 10 images ce n'est pas très significatif
-    
-    print(output.image.shape) # 10,1, 3, 64, 64
-    
-    # for each image, we need to evaluate the main colors
-    
-    for im in output.image:
-        nb_image +=1
+test_data = CUB('/home/asenella/scratch/data', split='test',max_lenght=32).text_data
+
+for path in os.listdir('/home/asenella/experiments/CUB'):
+    print( path)
+    path_model = os.path.join('/home/asenella/experiments/CUB',path,'final_model')
+    if os.path.exists(path_model):
+        print('starting evaluation')  
+        model = AutoModel.load_from_folder(path_model)
         
-        # Transform to PIL 
-        ndarr = (
-            im[0].mul(255)
-            .add_(0.5)
-            .clamp_(0, 255)
-            .permute(1, 2, 0)
-            .to("cpu", torch.uint8)
-            .numpy()
-        )
-        recon_image = Image.fromarray(ndarr).convert('HSV')
+        wandb_path = load_wandb_path_from_folder(path_model)
         
+        entity, project, run_id = tuple(wandb_path.split("/"))
         
-        hsv = np.array(recon_image)[:, :, :].copy()
-        
-        color_masks, color_count, most_present = count_pixels_per_color(hsv)
-        if color in most_present:
-            nb_coherent +=1
+        wandb_run = wandb.init(
+                 project="mmvae_plus_CUB", id=run_id, resume="must", reinit=True
+            )
+        # Generate sentences and transform it 
+        nb_coherent = 0
+        nb_image = 0
+        for color in ['white', 'yellow', 'red','blue', 'green','grey', 'brown', 'black']:
+            
+            sentence = f'This bird is completely {color}'
+            
+            tokenized = word_tokenize(sentence)
+            
+            # Then we need to transform to the same format as in the CUB dataset
+            tok = tokenized + ['<eos>']
+            length = len(tok)
+            pad_count = 0
+            if test_data.max_sequence_length > length:
+                tok.extend(['<pad>'] * (test_data.max_sequence_length - length))
+                pad_count += 1
+            idx = [test_data.w2i.get(w, test_data.w2i['<exc>']) for w in tok]
+            print(tokenized)
+            print(idx)
+            sent = torch.nn.functional.one_hot(torch.Tensor(idx).long(), test_data.vocab_size).float().unsqueeze(0)
+            
+            testing_set = MultimodalBaseDataset(data=dict(text = sent))
+            
+            # Visualize samples
+            vis_config = VisualizationConfig(n_samples=10,n_data_cond=1)
+            vis_module = Visualization(model,test_dataset=testing_set,output=f'./test/{color}', eval_config=vis_config)
+            
+            recon_image = vis_module.conditional_samples_subset(['text'],gen_mod='image')
+            
+            wandb_run.log(
+                {f"conditional_from_text": wandb.Image(recon_image)}
+            )
+
+            
+            # Compute coherence metric
+            model.eval()
+            output = model.predict(inputs = MultimodalBaseDataset(data=dict(text = sent)),cond_mod='text',gen_mod='image', N=10) # 10 images ce n'est pas très significatif
+            
+            print(output.image.shape) # 10,1, 3, 64, 64
+            
+            # for each image, we need to evaluate the main colors
+            
+            for im in output.image:
+                nb_image +=1
+                
+                # Transform to PIL 
+                ndarr = (
+                    im[0].mul(255)
+                    .add_(0.5)
+                    .clamp_(0, 255)
+                    .permute(1, 2, 0)
+                    .to("cpu", torch.uint8)
+                    .numpy()
+                )
+                recon_image = Image.fromarray(ndarr).convert('HSV')
+                
+                
+                hsv = np.array(recon_image)[:, :, :].copy()
+                
+                color_masks, color_count, most_present = count_pixels_per_color(hsv)
+                if color in most_present:
+                    nb_coherent +=1
 
     
-print('coherence :', nb_coherent/nb_image)
-        
+        print('coherence :', nb_coherent/nb_image)
+        wandb.log({'coherence': nb_coherent/nb_image})
+        wandb_run.finish()
+
         
 
 
