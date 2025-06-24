4bcc539f5752587388577a5b92b0d73c9e59a3f1
AgatheSenellart <agathe.senellart@gmail.com>
2025-06-24 10:17:58 +0200

    [version] update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="multivae",
-    version="1.0.0",
+    version="1.0.1",
     author="Agathe Senellart, Clement Chadebec",
     author_email="agathe.senellart@inria.fr",
     description="Unifying Generative Multimodel Variational Autoencoders in Pytorch",
