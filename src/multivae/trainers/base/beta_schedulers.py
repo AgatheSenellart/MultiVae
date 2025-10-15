3d2bda2628cd05210816be74bba4c72a11700656
AgatheSenellart <agathe.senellart@inria.fr>
2025-10-15 11:31:05 +0200

    add min beta in beta_scheduler

--- src/multivae/trainers/base/beta_schedulers.py
+++ src/multivae/trainers/base/beta_schedulers.py
@@ -3,14 +3,14 @@
 import numpy as np
 import math
 
-def frange_cycle_linear(n_epochs=100, n_cycle=4, ratio=0.5):
+def frange_cycle_linear(n_epochs=100, n_cycle=4, ratio=0.5, min_beta=0):
     L = np.ones(n_epochs)
     period = n_epochs/n_cycle
-    step = 1/(period*ratio) # linear schedule
+    step = (1-min_beta)/(period*ratio) # linear schedule
 
     for c in range(n_cycle):
 
-        v , i = 0 , 0
+        v , i = min_beta , 0
         while v <= 1 and (int(i+c*period) < n_epochs):
             L[int(i+c*period)] = v
             v += step
@@ -18,16 +18,16 @@ def frange_cycle_linear(n_epochs=100, n_cycle=4, ratio=0.5):
     return L    
 
 
-def frange_cycle_sigmoid(n_epochs=100, n_cycle=4, ratio=0.5):
+def frange_cycle_sigmoid(n_epochs=100, n_cycle=4, ratio=0.5, min_beta=0):
     L = np.ones(n_epochs)
     period = n_epochs/n_cycle
-    step = 1/(period*ratio) # step is in [0,1]
+    step = (1-min_beta)/(period*ratio) # step is in [0,1]
     
     # transform into [-6, 6] for plots: v*12.-6.
 
     for c in range(n_cycle):
 
-        v , i = 0 , 0
+        v , i = min_beta , 0
         while v <= 1:
             L[int(i+c*period)] = 1/(1.0+ np.exp(- (v*12.-6.)))
             v += step
@@ -37,25 +37,26 @@ def frange_cycle_sigmoid(n_epochs=100, n_cycle=4, ratio=0.5):
 
 #  function  = 1 − cos(a), where a scans from 0 to pi/2
 
-def frange_cycle_cosine( n_epoch, n_cycle=4, ratio=0.5):
-    L = np.ones(n_epoch)
-    period = n_epoch/n_cycle
-    step = 1/(period*ratio) # step is in [0,1]
+def frange_cycle_cosine( n_epochs=100, n_cycle=4, ratio=0.5, min_beta=0):
+    L = np.ones(n_epochs)
+    period = n_epochs/n_cycle
+    step = (1-min_beta)/(period*ratio) # step is in [0,1]
     
     # transform into [0, pi] for plots: 
 
     for c in range(n_cycle):
 
-        v , i = 0 , 0
+        v , i = min_beta, 0
         while v <= 1:
             L[int(i+c*period)] = 0.5-.5*math.cos(v*math.pi)
             v += step
             i += 1
     return L    
 
-def frange( step=1/30, n_epochs=100):
+def frange(n_epochs=100,n_augment=30, min_beta=0):
+    step = (1-min_beta)/n_augment
     L = np.ones(n_epochs)
-    v , i = 0 , 0
+    v , i = min_beta , 0
     while v <= 1:
         L[i] = v
         v += step
